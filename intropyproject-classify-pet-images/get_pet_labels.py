e39e0d136ac5c1e0b6346692c7d4693610db497c
jc-cmelgarejo <jcmelgarejo3@gmail.com>
2020-09-27 18:38:15 +0200

    feat: Create pet image labels

--- intropyproject-classify-pet-images/get_pet_labels.py
+++ intropyproject-classify-pet-images/get_pet_labels.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/get_pet_labels.py
 #                                                                             
-# PROGRAMMER: 
-# DATE CREATED:                                  
+# PROGRAMMER: Juan C. Melgarejo
+# DATE CREATED: 27/09/2020                                  
 # REVISED DATE: 
 # PURPOSE: Create the function get_pet_labels that creates the pet labels from 
 #          the image's filename. This function inputs: 
@@ -19,10 +19,6 @@
 # Imports python modules
 from os import listdir
 
-# TODO 2: Define get_pet_labels function below please be certain to replace None
-#       in the return statement with results_dic dictionary that you create 
-#       with this function
-# 
 def get_pet_labels(image_dir):
     """
     Creates a dictionary of pet labels (results_dic) based upon the filenames 
@@ -40,6 +36,10 @@ def get_pet_labels(image_dir):
       List. The list contains for following item:
          index 0 = pet image label (string)
     """
-    # Replace None with the results_dic dictionary that you created with this
-    # function
-    return None
+    file_names = listdir(image_dir)
+    results_dic = {}
+    for name in file_names:
+        value = ''.join([c for c in name[:-3] if c.isalpha() or c=='_']).replace('_',' ').strip().lower()
+        if name not in results_dic:
+            results_dic[name] = [value]
+    return results_dic
